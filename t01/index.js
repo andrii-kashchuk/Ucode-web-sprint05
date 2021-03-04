ce12647ce20cfe52193fdfa445169b36b5012c8c
Andrey <fullstack2021@gmail.com>
2021-03-04 21:43:19 +0200

    t00 and t01 is done, working on t02

---
+++ t01/index.js
@@ -0,0 +1,12 @@
+/*
+  Task 01
+  Task name: Iron Vars
+*/
+module.exports = {
+  ironMan: 'Anthony Edward "Tony" Stark',
+  warMachine: 'Colonel James Rupert "Rhodey" Rhodes',
+  var0: true,
+  var1: "string",
+  var2: 12.5,
+  var3: 1
+}
